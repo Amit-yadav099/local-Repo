21ac7c0aed7d9f157a75a4a17cf1689919072865
Amit-yadav099 <kartiktroy00001@gmail.com>
2025-04-07 15:15:17 +0530

    odd-even code upload

---
+++ even-odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"enter your number";
+cin>>num;
+if(num%2==0)
+cout<<" given no. is even";
+else
+cout<<"given no. is odd";
+
+
+}
